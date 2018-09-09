be6fc8b4d0da750e601591fa1a13522aac20de90
kosoto <kstad@naver.com>
2018-09-09 21:53:17 +0900

    180909

--- programmers/src/kakao/traffic.java
+++ programmers/src/kakao/traffic.java
@@ -27,7 +27,37 @@ public class traffic {
 		 }
 		 return answer;
 	  }	
+	 
 	 public static int solution2(String[] lines) {
+		 if(lines.length==1) return 1; 
+		 int answer = 1;
+		 int[][] table = new int[lines.length][2];
+		 //시작 시간 구하기 완료
+		 for(int i=0;i<lines.length;i++) {
+			 String times = lines[i].split(" ")[1];
+			 //시, 분을 계산한 초
+			 table[i][1] =  (Integer.parseInt(times.split(":")[0])*3600+Integer.parseInt(times.split(":")[1])*60)*1000
+					 +(int)((Double.parseDouble(times.split(":")[2]))*1000);
+			 table[i][0] = table[i][1] - (int)(Double.parseDouble((lines[i].split(":")[2].split(" "))[1].split("s")[0]) *1000) + 1;
+		 } 
+		 // 수정해야함
+		 int count;
+		 for(int i=0;i<lines.length;i++) {
+			 count = 1;
+			 for(int j=i+1;j<lines.length;j++) {
+				 if(table[j][0]<table[i][1] + 1000) {
+					 count++;
+					 answer = (answer>count)?answer:count;
+				 }else {
+					 answer = (answer>count)?answer:count;
+					 break;
+				 } 
+				 answer = (answer>count)?answer:count;
+			 }
+		 }
+		 return answer;
+	 }
+/*	 public static int solution2(String[] lines) {
 		 int answer = 1;
 		 double[][] table = new double[lines.length][2];
 		 //시작 시간 구하기 완료
@@ -72,12 +102,13 @@ public class traffic {
 		 
 		 return answer;
 	 }
-	 public static void main(String[] args) {
+*/	 public static void main(String[] args) {
 		 //String[] lines = {"2016-09-15 03:10:33.020 0.011s"};
+		String[] lines = {"2016-09-15 00:00:00.000 2.3s", "2016-09-15 23:59:59.999 0.1s"};
 		 //String[] lines = {"2016-09-15 01:00:04.001 2.0s", "2016-09-15 01:00:07.000 2s"};
 		// String[] lines = {"2016-09-15 01:00:04.002 2.0s", "2016-09-15 01:00:07.000 2s"};
-		 String[] lines = {"2016-09-15 20:59:57.421 0.351s", "2016-09-15 20:59:58.233 1.181s", "2016-09-15 20:59:58.299 0.8s", "2016-09-15 20:59:58.688 1.041s", "2016-09-15 20:59:59.591 1.412s", "2016-09-15 21:00:00.464 1.466s", "2016-09-15 21:00:00.741 1.581s", "2016-09-15 21:00:00.748 2.31s", "2016-09-15 21:00:00.966 0.381s", "2016-09-15 21:00:02.066 2.62s"};
-		 System.out.println("결과 : "+solution(lines));
+		 //String[] lines = {"2016-09-15 20:59:57.421 0.351s", "2016-09-15 20:59:58.233 1.181s", "2016-09-15 20:59:58.299 0.8s", "2016-09-15 20:59:58.688 1.041s", "2016-09-15 20:59:59.591 1.412s", "2016-09-15 21:00:00.464 1.466s", "2016-09-15 21:00:00.741 1.581s", "2016-09-15 21:00:00.748 2.31s", "2016-09-15 21:00:00.966 0.381s", "2016-09-15 21:00:02.066 2.62s"};
+		 System.out.println("결과 : "+solution2(lines));
 	}
 }
 
